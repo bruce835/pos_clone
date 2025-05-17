70cfa1916f41c48003c7c1d19da4bb29f0e8331a
Robin Wells <robinwells@Robins-Air.lan>
2025-05-17 08:36:05 -0400

    Added a new function to get input

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,47 @@
 use std::io;
 use std::io::Write;
 
-fn w_main() { 
-    println!("At Main:");
-    print!("Window: ");
+fn get_user_input(current_window: String) {
+    let current_window = current_window.trim();
+    let current_window = current_window.to_string(); 
+    print!("POS: ");
     io::stdout().flush().expect("Could not flush output buffer.");
-    let mut window = String::new();
-    io::stdin().read_line(&mut window).expect("Could not read line.");
-    let window = window;
-    change_window(window); 
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Could not read line.");
+    let input = input;
+
+    print!("{}", current_window);
+    if input.trim() != "current_window" {
+        change_window(input);
+    }
+    else {
+        change_window(current_window);
+    }
+}
+
+fn w_main() { 
+    let current_window = String::from("Main");
+    println!("At Main:");
+    get_user_input(current_window);
 }
 
 fn w_other() {
+    let current_window = String::from("Other");
     println!("At Other:");
+    get_user_input(current_window);
+}
+
+fn change_window(window: String) {
+    println!("Debug: change_window");
+   if window.trim() == "Other" {
+       w_other();
+   } 
+
+   else if window.trim() == "Main" {
+       w_main();
+   }
 }
 
 fn main() {
     w_main();
-}
-
-fn change_window(window: String) {
-   if window.trim() == "Other" {
-       w_other();
-   } 
 }
